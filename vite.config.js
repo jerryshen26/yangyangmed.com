3fc395b36fc339136442e073a1bd362cda0b923a
jerryshen26 <fast199111@yahoo.com.tw>
2023-04-11 13:13:15 +0800

    把base改成'/yangyangmed/'然後重新建置

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jerryshen26.github.io/yangyangmed.github.io/',
+  base: '/yangyangmed/',
   plugins: [vue()],
   resolve: {
     alias: {
