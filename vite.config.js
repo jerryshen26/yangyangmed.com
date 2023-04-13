f3c06b60ce4c1a06fc5d7862daab588b44b58ef0
jerryshen26 <fast199111@yahoo.com.tw>
2023-04-13 14:14:10 +0800

    新增rwd並且有重新建置的狀態

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: '/',
+  plugins: [vue(),sitemap({
+    baseURL: 'https://yangyangmed.com',
+    sitemapFilename: 'sitemap.xml',
+    pretty: true,
+    exclude: ['/404']
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
