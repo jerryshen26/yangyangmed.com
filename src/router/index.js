3755fe02467d45f32f48ee5debc37227cda6506c
jerryshen26 <fast199111@yahoo.com.tw>
2025-07-27 21:20:58 +0800

    feat:還原正常的專案

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue'
 import UsageScenarios from '../views/UsageScenarios.vue';
-import Products from '../views/Products.vue';
-import AboutView from '../views/AboutView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,18 +14,7 @@ const router = createRouter({
       path: '/usage-scenarios',
       name: 'UsageScenarios',
       component: UsageScenarios
-    },
-    {
-      path: '/products',
-      name: 'Products',
-      component: Products
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: AboutView
     }
-
   ]
 })
 
