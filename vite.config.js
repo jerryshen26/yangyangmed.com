a546dc664fb23c4584dc139062f59d6d4c05af42
jerryshen26 <fast199111@yahoo.com.tw>
2025-08-18 21:03:11 +0800

    feat:更新符合seo規範的版面

--- vite.config.js
+++ vite.config.js
@@ -2,32 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
-    vue(),
-    sitemap({
-      base: 'https://yangyangmed.com',
-      filename: 'sitemap.xml',
-      hostname: 'https://yangyangmed.com',
-      routes: [
-        {
-          path: '/',
-          lastMod: new Date(),
-          priority: 1.0,
-          changefreq: 'daily'
-        },
-        {
-          path: '/index',
-          lastMod: new Date(),
-          priority: 0.8,
-          changefreq: 'daily'
-        }
-      ]
-    })
+    vue()
   ],
   resolve: {
     alias: {
